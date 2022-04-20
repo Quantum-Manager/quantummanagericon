c885510a6f03fb40f9e37db02e9e0135b6117410
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2022-04-20 18:31:51 +0300

    update script installer

--- script.php
+++ script.php
@@ -17,22 +17,6 @@ defined('_JEXEC') or die;
  */
 class plgQuickiconQuantummanagericonInstallerScript
 {
-	/**
-	 * Constructor
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 */
-	public function __construct(JAdapterInstance $adapter) {}
-
-	/**
-	 * Called before any type of action
-	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function preflight($route, JAdapterInstance $adapter) {}
 
 	/**
 	 * Called after any type of action
@@ -42,35 +26,11 @@ class plgQuickiconQuantummanagericonInstallerScript
 	 *
 	 * @return  boolean  True on success
 	 */
-	public function postflight($route, JAdapterInstance $adapter) {
+	public function postflight($route, $adapter) {
 		$db = JFactory::getDbo();
 		$query = $db->getQuery( true );
 		$query->update( '#__extensions' )->set( 'enabled=1' )->where( 'type=' . $db->q( 'plugin' ) )->where( 'element=' . $db->q( 'quantummanagericon' ) );
 		$db->setQuery( $query )->execute();
 	}
 
-	/**
-	 * Called on installation
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function install(JAdapterInstance $adapter) {}
-
-	/**
-	 * Called on update
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function update(JAdapterInstance $adapter) {}
-
-	/**
-	 * Called on uninstallation
-	 *
-	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
-	 */
-	public function uninstall(JAdapterInstance $adapter) {}
 }
